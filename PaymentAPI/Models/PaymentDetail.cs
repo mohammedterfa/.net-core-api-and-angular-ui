ff2e4155d2d9659eeb5aa24ce17ab3870d19e1bc
mohammedterfa <mohammedterfa@gmail.com>
2024-01-08 13:00:31 +0300

    set ConnectionStrings

--- PaymentAPI/Models/PaymentDetail.cs
+++ PaymentAPI/Models/PaymentDetail.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PaymentAPI.Models
 {
@@ -6,9 +7,13 @@ namespace PaymentAPI.Models
 	{
 		[Key]
 		public int PaymentDetailId { get; set; }
-		public string CardOwnerName { get; set; }
-		public string CardNumber { get; set; }
-		public string ExpirationDate { get; set; }
-		public string SecurityCode { get; set; }
+		[Column(TypeName="nvarchar(100)")]
+		public string CardOwnerName { get; set; } = "";
+		[Column(TypeName = "nvarchar(16)")]
+		public string CardNumber { get; set; } = "";
+		[Column(TypeName = "nvarchar(5)")]
+		public string ExpirationDate { get; set; } = "";
+		[Column(TypeName = "nvarchar(3)")]
+		public string SecurityCode { get; set; } = "";
 	}
 }
