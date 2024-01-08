3f3725637dd73c23c6852ebc3d5c373e8f536d8b
mohammedterfa <mohammedterfa@gmail.com>
2024-01-08 12:39:34 +0300

    first commit

---
+++ PaymentAPI/Models/PaymentDetail.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PaymentAPI.Models
+{
+	public class PaymentDetail
+	{
+		[Key]
+		public int PaymentDetailId { get; set; }
+		public string CardOwnerName { get; set; }
+		public string CardNumber { get; set; }
+		public string ExpirationDate { get; set; }
+		public string SecurityCode { get; set; }
+	}
+}
